a9cddd67287d5d52f40a057da1deee1d4acc14c1
User <sphurti.tilekar09@gmail.com>
2025-11-03 22:32:29 +0530

    Print

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,4 +1,10 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
+
+      System.out.println("--- Login Test ---");
+
 }
